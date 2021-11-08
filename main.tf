ecefd94bb5e4349604eb718ee883ff1086bf2cb8
Walid El Sayed Aly <walid.el-sayed-aly@deutschebahn.com>
2021-11-08 23:25:21 +0100

    chenge block

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source     = "hashicorp/aws"
-      version    = "~> 3.27"
-      access_key = "${var.AWS_ACCESS_KEY_ID}"
-      secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
   }
 
@@ -12,8 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "eu-central-1"
+  profile    = "default"
+  region     = "eu-central-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "app_server" {
