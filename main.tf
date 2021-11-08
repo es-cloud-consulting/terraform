c3c469d419574d0240737e5a87fae1a84b5908da
Walid El Sayed Aly <walid.el-sayed-aly@deutschebahn.com>
2021-11-08 23:24:04 +0100

    fmt

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      source     = "hashicorp/aws"
+      version    = "~> 3.27"
       access_key = "${var.AWS_ACCESS_KEY_ID}"
       secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
     }
